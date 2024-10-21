8211042c6871f3ddfdf51f345f91452c7820578f
Anurag Mishra <115824470+AnuragMishra13@users.noreply.github.com>
2024-10-21 19:19:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            This is my First React project - Anurag Mishra
+            This is my First React project - Yashodha Beldar
         </p>
       </header>
     </div>
